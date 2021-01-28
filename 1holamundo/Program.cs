97712a7c20b4a1588fb89b6eb542f902e3b562cf
FcoJLunaLuis <fcojlunaluis@gmail.com>
2021-01-28 11:30:22 -0600

    Se agregaron los programas del 1 al 4

---
+++ 1holamundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Programacion C#");
+        }
+    }
+}
---
+++ 4pagatrabajador/Program.cs
@@ -0,0 +1,35 @@
+﻿//Paga de trabajador
+using System;
+
+namespace _4pagatrabajador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int horas;
+            float paga, tasa = 0.10F;
+
+            float impuesto, pagabruta, paganeta;
+
+            Console.WriteLine("Calculo de la paga de un trabajador");
+
+            Console.Write("Nombre del trabajador"); nombre = Console.ReadLine();
+            Console.Write("Horas trabajadas    :"); horas = int.Parse(Console.ReadLine());
+            Console.Write("Paga por hora       :"); paga = float.Parse(Console.ReadLine());
+
+            pagabruta = horas * paga;
+            impuesto = pagabruta * tasa;
+            paganeta = pagabruta - impuesto;
+
+            Console.Write("\n\n");
+            Console.WriteLine("Estimado " + nombre);
+            Console.WriteLine($"Trabajaste {horas} horas, con una paga por hora de {paga}\n");
+            Console.WriteLine("Paga Bruta : {0}", pagabruta);
+            Console.WriteLine("Impuesto : {0}", impuesto);
+            Console.WriteLine("Paga Neta : {0}", paganeta);
+
+        }
+    }
+}
